ff5cf7c8688bb7a9658230b3f46ddafe0b2121cc
connerng <cjngadisastra@gmail.com>
2024-06-12 20:12:19 -0700

    Finished LR

---
+++ main.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+df = pd.read_csv('diabetes_data.csv')
+y = df['Diagnosis']
+x = df.drop('Diagnosis', axis=1)
+x = x.drop('DoctorInCharge', axis=1)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=61)
+
+lr = LinearRegression()
+lr.fit(x_train, y_train)
+
+y_lr_train_pred = lr.predict(x_train)
+y_lr_test_pred = lr.predict(x_test)
+
+lr_train_mse = mean_squared_error(y_train, y_lr_train_pred)
+lr_train_r2 = r2_score(y_train, y_lr_train_pred)
+
+lr_test_mse = mean_squared_error(y_test, y_lr_test_pred)
+lr_test_r2 = r2_score(y_test, y_lr_test_pred)
+
+print('Training MSE: ', lr_train_mse)
+print('Training R2: ', lr_train_r2)
+print('Test MSE: ', lr_test_mse)
+print('Test R2: ', lr_test_r2)
+
+lr_results = pd.DataFrame(['Linear Regression', lr_train_mse, lr_train_r2, lr_test_mse, lr_test_r2]).transpose()
+lr_results.columns = ['Method', 'Training MSE', 'Training R^2', 'Test MSE', 'Test R^2']
+
